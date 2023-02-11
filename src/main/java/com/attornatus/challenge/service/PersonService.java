8b0a4163b2d4ede303f72d30868933e08b8670ba
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-11 13:40:21 -0300

    feat: save person method

--- src/main/java/com/attornatus/challenge/service/PersonService.java
+++ src/main/java/com/attornatus/challenge/service/PersonService.java
@@ -2,6 +2,9 @@ package com.attornatus.challenge.service;
 
 import org.springframework.stereotype.Service;
 
+import com.attornatus.challenge.entity.Person;
+import com.attornatus.challenge.repository.PersonRepository;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +13,11 @@ import lombok.Data;
 @AllArgsConstructor
 public class PersonService {
 	
+	private PersonRepository personRepository;
 	
+	public Person savePerson(Person person) {
+		personRepository.save(person);
+		return person;
+	}
 	
 }
