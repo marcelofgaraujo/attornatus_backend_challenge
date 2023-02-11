1f696d0195550afd9f360c5c01003699d270cfb1
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-11 20:47:53 -0300

    feat: create a person endpoint

--- src/main/java/com/attornatus/challenge/controller/PersonController.java
+++ src/main/java/com/attornatus/challenge/controller/PersonController.java
@@ -2,9 +2,12 @@ package com.attornatus.challenge.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +36,10 @@ public class PersonController {
 	@GetMapping
 	public ResponseEntity<Person> getPersonById(@PathVariable Long personId) {
 		return ResponseEntity.ok(personService.findPersonById(personId));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Person> createAPerson(@RequestBody Person person) {
+		return new ResponseEntity<Person>(personService.savePerson(person), HttpStatus.CREATED);
 	}
 }
