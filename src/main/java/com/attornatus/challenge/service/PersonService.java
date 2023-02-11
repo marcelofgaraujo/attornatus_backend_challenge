b03ab8099ac3cb6dae428f4bcfc7d1d6b811c15e
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-11 13:49:11 -0300

    feat: find person by id

--- src/main/java/com/attornatus/challenge/service/PersonService.java
+++ src/main/java/com/attornatus/challenge/service/PersonService.java
@@ -2,7 +2,9 @@ package com.attornatus.challenge.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.attornatus.challenge.entity.Person;
 import com.attornatus.challenge.repository.PersonRepository;
@@ -24,6 +26,12 @@ public class PersonService {
 	
 	public List<Person> getAllPersons() {
 		return personRepository.findAll();
+	}
+	
+	public Person getPersonById(long personId) {
+		return personRepository.findById(personId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
+						"Pessoa não encontrada!"));
 	}
 	
 }
