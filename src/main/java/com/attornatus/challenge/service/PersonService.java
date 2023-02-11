ec2fce28729725be75e551bd656511dd45a703c4
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-11 13:37:23 -0300

    feat: person service initial annotations

---
+++ src/main/java/com/attornatus/challenge/service/PersonService.java
@@ -0,0 +1,15 @@
+package com.attornatus.challenge.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Service
+@Data
+@AllArgsConstructor
+public class PersonService {
+	
+	
+	
+}
