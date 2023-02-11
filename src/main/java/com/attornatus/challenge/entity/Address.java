ece1c18a52cb4950ee1894d516c7127ee3080792
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-11 11:26:21 -0300

    feat: new address entity and initial annotations

---
+++ src/main/java/com/attornatus/challenge/entity/Address.java
@@ -0,0 +1,38 @@
+package com.attornatus.challenge.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@Table
+@Entity
+public class Address {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "addressId")
+	private Long id;
+	
+	@Column
+	private String publicArea;
+	
+	@Column(length = 9)
+	private String CEP;
+	
+	@Column
+	private int number;
+	
+	@Column
+	private String city;
+	
+}
