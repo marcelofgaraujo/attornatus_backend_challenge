10b6f35fac7f685b94697ae84c54988d827cc047
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-11 21:26:03 -0300

    feat: find addresses by person id

--- src/main/java/com/attornatus/challenge/service/AddressService.java
+++ src/main/java/com/attornatus/challenge/service/AddressService.java
@@ -1,6 +1,8 @@
 package com.attornatus.challenge.service;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -24,5 +26,14 @@ public class AddressService {
 	
 	public List<Address> findAllAddresses() {
 		return addressRepository.findAll();
+	}
+	
+	public List<Address> findPersonAddresses(Long personId) {
+		Predicate<Address> addressesFilteredById = ad -> ad.getPerson().getId() == personId;
+		List<Address> addresses = findAllAddresses();
+		List<Address> personAddresses = addresses.stream()
+				.filter(addressesFilteredById)
+				.collect(Collectors.toList());
+		return personAddresses;
 	}
 }
