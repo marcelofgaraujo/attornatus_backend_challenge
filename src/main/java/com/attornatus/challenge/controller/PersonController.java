4cfe5a7caaac2035e4690a77eab81e1aa15a6f3a
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-11 21:00:05 -0300

    fix: added exact endpoints information

--- src/main/java/com/attornatus/challenge/controller/PersonController.java
+++ src/main/java/com/attornatus/challenge/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,12 +29,12 @@ public class PersonController {
 		return ResponseEntity.ok(personService.findAllPersons());
 	}
 	
-	@GetMapping
+	@GetMapping("/names/{name}")
 	public ResponseEntity<List<Person>> getPersonByName(@PathVariable String name) {
 		return ResponseEntity.ok(personService.findPersonsByName(name));
 	}
 	
-	@GetMapping
+	@GetMapping("/{personId}")
 	public ResponseEntity<Person> getPersonById(@PathVariable Long personId) {
 		return ResponseEntity.ok(personService.findPersonById(personId));
 	}
@@ -42,4 +43,5 @@ public class PersonController {
 	public ResponseEntity<Person> createAPerson(@RequestBody Person person) {
 		return new ResponseEntity<Person>(personService.savePerson(person), HttpStatus.CREATED);
 	}
+	
 }
