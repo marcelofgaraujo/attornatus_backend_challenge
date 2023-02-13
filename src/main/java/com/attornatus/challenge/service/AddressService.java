38777d1072480d4ae0063cfd44e0fcea94d0f8fe
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-13 13:07:30 -0300

    fix: add address to a person not working

--- src/main/java/com/attornatus/challenge/service/AddressService.java
+++ src/main/java/com/attornatus/challenge/service/AddressService.java
@@ -2,8 +2,6 @@ package com.attornatus.challenge.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -21,60 +19,66 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class AddressService {
-	
+
 	private AddressRepository addressRepository;
 	private PersonRepository personRepository;
-	
+
 	public Address saveAddress(Address address) {
 		addressRepository.save(address);
 		return address;
 	}
-	
+
 	public List<Address> findAllAddresses() {
 		return addressRepository.findAll();
 	}
-	
+
 	public Address findAddressById(Long addressId) {
-		return addressRepository.findById(addressId).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Endereço não encontrado!"));
+		return addressRepository.findById(addressId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Endereço não encontrado!"));
 	}
-	
-	public List<Address> findPersonAddresses(Long personId) {
-		Predicate<Address> addressesFilteredById = ad -> ad.getPerson().getId() == personId;
-		List<Address> addresses = findAllAddresses();
-		List<Address> personAddresses = addresses.stream()
-				.filter(addressesFilteredById)
-				.collect(Collectors.toList());
-		return personAddresses;
+
+	public List<Address> findPersonAddresses(Long personId) throws ResponseStatusException {
+		Optional<Person> personOpt = personRepository.findById(personId);
+
+		if (personOpt.isPresent()) {
+			Person person = personOpt.get();
+			return person.getAddresses();
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pessoa não encontrada!");
+		}
 	}
-	
+
 	public Address addAddressToAPerson(Long personId, Long addressId) throws ResponseStatusException {
 		Optional<Person> personOpt = personRepository.findById(personId);
 		Address address = findAddressById(addressId);
-		
-		if(personOpt.isPresent()) {
+
+		if (personOpt.isPresent()) {
 			Person person = personOpt.get();
 			person.getAddresses().add(address);
+			address.setPerson(person);
+			personRepository.save(person);
 			return address;
 		} else {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pessoa não encontrada!");
 		}
 	}
-	
+
 	public Address setPrincipalAddress(Long personId, Long addressId) throws ResponseStatusException {
 		Optional<Person> personOpt = personRepository.findById(personId);
-		if(personOpt.isPresent()) {
+		if (personOpt.isPresent()) {
 			Person person = personOpt.get();
 			Address address = findAddressById(addressId);
 			person.setPrincipalAddress(address);
-			return address;
+			address.setPerson(person);
+			return person.getPrincipalAddress();
 		} else {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pessoa não encontrada!");
 		}
 	}
-	
+
 	public Address findPrincipalAddress(Long personId) throws ResponseStatusException {
 		Optional<Person> personOpt = personRepository.findById(personId);
-		if(personOpt.isPresent()) {
+		if (personOpt.isPresent()) {
 			return personOpt.get().getPrincipalAddress();
 		} else {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pessoa não encontrada!");
