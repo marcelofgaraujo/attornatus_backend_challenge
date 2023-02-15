522e84fdc269671e64a336d051c77651353e174b
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-14 21:46:26 -0300

    feat: delete person endpoint

--- src/main/java/com/attornatus/challenge/controller/PersonController.java
+++ src/main/java/com/attornatus/challenge/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,13 @@ public class PersonController {
 	@PutMapping("/{personId}")
 	public ResponseEntity<Person> updatePerson(@PathVariable Long personId, @RequestBody Person updatedPerson) {
 		return ResponseEntity.ok(personService.updatePerson(personId, updatedPerson));
+	}
+	
+	@ApiOperation(value = "Excluir uma pessoa")
+	@DeleteMapping("/{personId}")
+	public ResponseEntity<Void> deletePerson(@PathVariable Long personId) {
+		personService.deletePersonById(personId);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
