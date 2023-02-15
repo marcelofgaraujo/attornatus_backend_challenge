2267547058f4a92c1483a8ecb3b343925cf33a9e
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-15 02:59:19 -0300

    feat: addressDTO converter methods and changes in returns

--- src/main/java/com/attornatus/challenge/controller/AddressController.java
+++ src/main/java/com/attornatus/challenge/controller/AddressController.java
@@ -1,7 +1,9 @@
 package com.attornatus.challenge.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.attornatus.challenge.dto.AddressDTO;
 import com.attornatus.challenge.entity.Address;
 import com.attornatus.challenge.service.AddressService;
 
@@ -27,53 +30,64 @@ import lombok.AllArgsConstructor;
 public class AddressController {
 	
 	private AddressService addressService;
+	private ModelMapper modelMapper;
 	
 	@ApiOperation(value = "Buscar todos os endereços")
 	@GetMapping
-	public ResponseEntity<List<Address>> getAllAddresses() {
-		return ResponseEntity.ok(addressService.findAllAddresses());
+	public ResponseEntity<List<AddressDTO>> getAllAddresses() {
+		List<Address> addresses = addressService.findAllAddresses();
+		return ResponseEntity.ok(convertListAddressToListDTO(addresses));
 	}
 	
 	@ApiOperation(value = "Buscar endereço por id")
 	@GetMapping("/{addressId}")
-	public ResponseEntity<Address> getAddressById(@PathVariable Long addressId) {
-		return ResponseEntity.ok(addressService.findAddressById(addressId));
+	public ResponseEntity<AddressDTO> getAddressById(@PathVariable Long addressId) {
+		Address address = addressService.findAddressById(addressId);
+		return ResponseEntity.ok(convertAddressToDTO(address));
 	}
 	
 	@ApiOperation(value = "Adicionar um endereço a uma pessoa")
 	@GetMapping("/add/{personId}/{addressId}")
-	public ResponseEntity<Address> addPersonAddress(@PathVariable Long personId, @PathVariable Long addressId) {
-		return ResponseEntity.ok(addressService.addAddressToAPerson(personId, addressId));
+	public ResponseEntity<AddressDTO> addPersonAddress(@PathVariable Long personId, @PathVariable Long addressId) {
+		Address address = addressService.addAddressToAPerson(personId, addressId);
+		return ResponseEntity.ok(convertAddressToDTO(address));
 	}
 	
 	@ApiOperation(value = "Definir um endereço principal a uma pessoa")
 	@GetMapping("/setprincipaladdress/{personId}/{addressId}")
-	public ResponseEntity<Address> addPrincipalAddress(@PathVariable Long personId, @PathVariable Long addressId) {
-		return ResponseEntity.ok(addressService.setPrincipalAddress(personId, addressId));
+	public ResponseEntity<AddressDTO> addPrincipalAddress(@PathVariable Long personId, @PathVariable Long addressId) {
+		Address address = addressService.setPrincipalAddress(personId, addressId);
+		return ResponseEntity.ok(convertAddressToDTO(address));
 	}
 	
 	@ApiOperation(value = "Buscar endereço principal de uma pessoa")
 	@GetMapping("/principaladdress/{personId}")
-	public ResponseEntity<Address> getPrincipalAddress(@PathVariable Long personId) {
-		return ResponseEntity.ok(addressService.findPrincipalAddress(personId));
+	public ResponseEntity<AddressDTO> getPrincipalAddress(@PathVariable Long personId) {
+		Address address = addressService.findPrincipalAddress(personId);
+		return ResponseEntity.ok(convertAddressToDTO(address));
 	}
 	
 	@ApiOperation(value = "Buscar todos os endereços de uma pessoa")
 	@GetMapping("/useraddresses/{personId}")
-	public ResponseEntity<List<Address>> getAllPersonAddresses(@PathVariable Long personId) {
-		return ResponseEntity.ok(addressService.findPersonAddresses(personId));
+	public ResponseEntity<List<AddressDTO>> getAllPersonAddresses(@PathVariable Long personId) {
+		List<Address> addresses = addressService.findPersonAddresses(personId);
+		return ResponseEntity.ok(convertListAddressToListDTO(addresses));
 	}
 	
 	@ApiOperation(value = "Criar um endereço")
 	@PostMapping
-	public ResponseEntity<Address> createAnAddress(@RequestBody Address address) {
-		return new ResponseEntity<Address>(addressService.saveAddress(address), HttpStatus.CREATED);	
+	public ResponseEntity<AddressDTO> createAnAddress(@RequestBody AddressDTO addressDTO) {
+		Address address = convertDTOtoAddress(addressDTO);
+		addressService.saveAddress(address);
+		return new ResponseEntity<AddressDTO>(convertAddressToDTO(address), HttpStatus.CREATED);	
 	}
 	
 	@ApiOperation(value = "Editar um endereço")
 	@PutMapping("/{addressId}")
-	public ResponseEntity<Address> updateAddress(@PathVariable Long addressId, @RequestBody Address updatedAddress) {
-		return ResponseEntity.ok(addressService.updateAddress(addressId, updatedAddress));
+	public ResponseEntity<AddressDTO> updateAddress(@PathVariable Long addressId, @RequestBody AddressDTO updatedAddressDTO) {
+		Address updatedAddress = convertDTOtoAddress(updatedAddressDTO);
+		addressService.updateAddress(addressId, updatedAddress);
+		return ResponseEntity.ok(convertAddressToDTO(updatedAddress));
 	}
 	
 	@ApiOperation(value = "Excluir um endereço")
@@ -81,6 +95,23 @@ public class AddressController {
 	public ResponseEntity<Void> deleteAddress(@PathVariable Long addressId) {
 		addressService.deleteAddressById(addressId);
 		return ResponseEntity.noContent().build();
+	}
+	
+	private AddressDTO convertAddressToDTO(Address address) {
+		return modelMapper.map(address, AddressDTO.class);
+	}
+	
+	private Address convertDTOtoAddress(AddressDTO addressDTO) {
+		return modelMapper.map(addressDTO, Address.class);
+	}
+	
+	private List<AddressDTO> convertListAddressToListDTO(List<Address> addresses) {
+		List<AddressDTO> addressesDTO = new ArrayList<>();
+		addresses.forEach(ad -> {
+			addressesDTO.add(convertAddressToDTO(ad));
+		});
+		
+		return addressesDTO;
 	}
 
 }
