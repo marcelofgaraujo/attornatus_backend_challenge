d06c7e04ef09a39b363944ad186cd5119c34d077
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-15 19:21:01 -0300

    test: find persons by name

--- src/test/java/com/attornatus/challenge/service/PersonServiceTest.java
+++ src/test/java/com/attornatus/challenge/service/PersonServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.verify;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.attornatus.challenge.entity.Address;
 import com.attornatus.challenge.entity.Person;
 import com.attornatus.challenge.repository.PersonRepository;
 
@@ -63,6 +63,32 @@ class PersonServiceTest {
 		verify(personRepository).findAll();
 		assertEquals(listReturn.size(), 2);
 		assertNotNull(listReturn);
+	}
+	
+	@Test
+	void findPersonByIdTest() {
+		// arrange
+		Mockito.when(personRepository.findById(1L)).thenReturn(Optional.of(testPerson));
+		// action
+		Person personReturn = personService.findPersonById(1L);
+		// assert
+		verify(personRepository).findById(1L);
+		assertEquals(personReturn.getName(), "joão das couves");
+	}
+	
+	@Test
+	void findPersonByName() {
+		// arrange
+		Person newPerson = new Person();
+		newPerson.setName("joão das couves");
+		List<Person> personsList = Arrays.asList(testPerson, newPerson);
+		Mockito.when(personRepository.findByName("joão das couves")).thenReturn(personsList);
+		// action
+		List<Person> returnList = personService.findPersonsByName("joão das couves");
+		// assert
+		verify(personRepository).findByName("joão das couves");
+		assertEquals(returnList.size(), personsList.size());
+		assertNotNull(returnList);
 	}
 
 }
