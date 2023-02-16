cbec6fc3cb302a6d2b01d5442e1bc5df12483a45
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-02-16 19:02:12 -0300

    doc: address DTO comments

--- src/main/java/com/attornatus/challenge/dto/AddressDTO.java
+++ src/main/java/com/attornatus/challenge/dto/AddressDTO.java
@@ -1,3 +1,6 @@
+// um objeto address dto que retorna os atributos da entidade address sem que haja interação com as relações
+// retorna id, publicArea, cep, number e city
+
 package com.attornatus.challenge.dto;
 
 import io.swagger.annotations.ApiModelProperty;
